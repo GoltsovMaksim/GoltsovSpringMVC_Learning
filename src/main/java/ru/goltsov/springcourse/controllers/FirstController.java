5928de640ffabc7c1b4ab37f182d21138d93dbbf
Maksim Goltsov <mgoltsov@bk.ru>
2022-11-05 18:16:00 +0300

    Thymeleaf added and Model`s been inserted

--- src/main/java/ru/goltsov/springcourse/controllers/FirstController.java
+++ src/main/java/ru/goltsov/springcourse/controllers/FirstController.java
@@ -1,6 +1,7 @@
 package ru.goltsov.springcourse.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,8 +11,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class FirstController {
     @GetMapping("/hello")
     public String helloPage(@RequestParam(value = "name", required = false) String name,
-                            @RequestParam(value = "surname", required = false) String surname) {
-        System.out.println("Hello! " + name + " " + surname);
+                            @RequestParam(value = "surname", required = false) String surname, Model model) {
+//        System.out.println("Hello! " + name + " " + surname);
+        model.addAttribute("message", "Вот тебе! " + name + " " + surname);
         return "first/hello";
     }
 
