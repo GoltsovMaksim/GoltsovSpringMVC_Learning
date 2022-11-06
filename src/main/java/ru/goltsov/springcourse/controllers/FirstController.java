5ef6e985bd46c6684d493a25ef58b97e479b2e58
Maksim Goltsov <mgoltsov@bk.ru>
2022-11-06 16:55:53 +0300

    Simple web calculator has been added

--- src/main/java/ru/goltsov/springcourse/controllers/FirstController.java
+++ src/main/java/ru/goltsov/springcourse/controllers/FirstController.java
@@ -13,12 +13,28 @@ public class FirstController {
     public String helloPage(@RequestParam(value = "name", required = false) String name,
                             @RequestParam(value = "surname", required = false) String surname, Model model) {
 //        System.out.println("Hello! " + name + " " + surname);
-        model.addAttribute("message", "Вот тебе! " + name + " " + surname);
+        model.addAttribute("message", "Hello! " + name + " " + surname);
         return "first/hello";
     }
 
     @GetMapping("goodbye")
     public String goodbayPage() {
         return "first/goodbye";
+    }
+
+    @GetMapping("/calculator")
+    public String calculatorPage(@RequestParam(value = "a", required = false) int a,
+                                 @RequestParam(value = "b", required = false) int b,
+                                 @RequestParam(value = "c", required = false) String action,
+                                 Model model) {
+        double result = switch (action) {
+            case "multiplication" -> a * b;
+            case "addition" -> a + b;
+            case "substruction" -> a - b;
+            case "division" -> (double) a / (double) b;
+            default -> 0;
+        };
+        model.addAttribute("return","Hello! Answer is " + result);
+            return "first/calculator";
     }
 }
